3282eacd97ccb91cd4ba6eaa7e8f83d49bb402dc
Drin Musa <drinmusa99@hotmail.com>
2022-03-03 15:20:51 +0100

    Added base url for api call

--- app/server.js
+++ app/server.js
@@ -31,7 +31,9 @@ require("./middlewares/passport")(passport);
 app.use("/api/user", userRouter);
 app.use("/api/list", listRouter);
 app.use("/api/task", taskRouter);
-
+app.get('/',(req,res)=>{
+	res.send("Welcome to todo api");
+});
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
